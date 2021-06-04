803e01367ae29b1718f7eaf08eeb21acba338919
RetherP <vbelya07@gmail.com>
2021-06-04 20:06:56 +0200

    Setting everything acessable

--- src/com/example/gui.java
+++ src/com/example/gui.java
@@ -8,32 +8,38 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
 public class gui {
+    static JFrame f;
+    static JTextField title;
+    static JTextField username;
+    static JTextField pw;
+    static JTextField userinut;
+    static JTextField pwinput;
     public gui(){
         //frames
-        JFrame f = new JFrame("ATM machine");
+        f = new JFrame("ATM machine");
         f.setName("Atm machine");
         f.setSize(400,200);
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 
         //text fields
-        JTextField title = new JTextField("Please login");
+        title = new JTextField("Please login");
         title.setBounds(150, 0, 200, 20);
         title.setEditable(false);
         title.setBorder(BorderFactory.createEmptyBorder());
 
-        JTextField username= new JTextField("username:");
+        username= new JTextField("username:");
         username.setBounds(0, 50, 60,20);
         username.setEditable(false);
-
         username.setBorder(BorderFactory.createEmptyBorder());
-        JTextField pw= new JTextField("password:");
+
+        pw= new JTextField("password:");
         pw.setBounds(0, 100, 60,20);
         pw.setEditable(false);
         pw.setBorder(BorderFactory.createEmptyBorder());
 
         //input
-        JTextField userinut= new JTextField("Enter your username");
+        userinut= new JTextField("Enter your username");
         userinut.setBounds(80,50, 150, 20);
         userinut.setBorder(BorderFactory.createLineBorder(Color.BLACK));
         userinut.addMouseListener(new MouseAdapter() {
@@ -42,7 +48,7 @@ public class gui {
                 userinut.setText("");
             }
         });
-        JTextField pwinput= new JTextField("Enter your password");
+        pwinput= new JTextField("Enter your password");
         pwinput.setBounds(80,100,150,20);
         pwinput.setBorder(BorderFactory.createLineBorder(Color.BLACK));
         pwinput.addMouseListener(new MouseAdapter() {
